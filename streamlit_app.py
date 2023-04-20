a32f906c3a50263246fd82ca9b9fa4bd42f1972e
malek-zz <a.elguesaoui@gmail.com>
2023-04-20 21:35:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,39 @@
 import streamlit as st
 from PIL import Image
 
-image = Image.open('pdf.png')
+pdf = Image.open('pdf.png')
+pdf = Image.open('docx.png')
 
 
 col0, col1, col2, col3 = st.columns([0.1,3,3,3],gap="small")
 
 with col0:
-   st.subheader("test")
-   st.image(image, width=15)
+   st.subheader(" ")
+   st.image(pdf, width=15)
+   st.image(pdf, width=15)
+   st.image(pdf, width=15)
+   st.image(pdf, width=15)
+   st.image(pdf, width=15)
+   st.image(pdf, width=15)
+   st.image(pdf, width=15)
+   st.image(pdf, width=15)
+   st.image(pdf, width=15)
+   st.image(pdf, width=15)
+   st.image(docx, width=15)
 with col1:
    st.subheader("Documents")
-   st.text('This is some text. This is some text. This is some text.')
-   st.text('This is some text.')
-   st.text('This is some text.')
-   st.text('This is some text.')
-   st.text('This is some text.')
-   st.text('This is some text.')
-   st.text('This is some text.')
-   st.text('This is some text.')
-
+   st.text('Corporate Social Responsibility Policy.pdf')
+   st.text('Standard Corporate Social Responsibility Policy.pdf')
+   st.text('Activities in Corporate Social Responsibility Policy.pdf')
+   st.text('Environment Protection Program Policy.pdf')
+   st.text('Work Health and Safety Culture Policy.pdf')
+   st.text('Client Complaint Management Policy.pdf')
+   st.text('Basic Complaint Management Policy.pdf')
+   st.text('Anti-Harassement Policy and Procedure.pdf')
+   st.text('General Information Technology Policy.pdf')
+   st.text('Nondiscrimination Policy.pdf')
+   st.text('Air - Environment protection policy.docx')
+   
 with col2:
    st.subheader("Topics")
    st.text('This is some text.')
