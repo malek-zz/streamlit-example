ffefa2c4db259a262ebfd55abb1a138bd43cfa45
malek-zz <a.elguesaoui@gmail.com>
2023-04-20 15:48:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,13 @@ col1, col2, col3 = st.columns(3)
 with col1:
    st.header("Documents")
    st.text('This is some text.')
+   st.text('This is some text.')
+   st.text('This is some text.')
+   st.text('This is some text.')
+   st.text('This is some text.')
+   st.text('This is some text.')
+   st.text('This is some text.')
+   st.text('This is some text.')
 
 with col2:
    st.header("Topics")
