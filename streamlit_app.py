e89959353a7f50373f83dca18e9673eaae69a660
malek-zz <a.elguesaoui@gmail.com>
2023-04-20 23:35:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,13 +20,14 @@ files =['Corporate Social Responsibility Policy.pdf','Standard Corporate Social 
        'Environment Protection Program Policy.pdf','Work Health and Safety Culture Policy.pdf','Client Complaint Management Policy.pdf',
        'Basic Complaint Management Policy.pdf','Anti-Harassement Policy and Procedure.pdf','General Information Technology Policy.pdf','Nondiscrimination Policy.pdf',
        'Air - Environment protection policy.docx']
-cols = st.columns([0.3,8,3,3])
+colsize = [0.3,5,2,2]
+cols = st.columns(colsize)
 cols[0].write("")
 cols[1].write("Documents")
 cols[2].write('Topic')
 cols[3].write('Content')
 for i in files:
-    cols = st.columns([0.3,5,2,2])
+    cols = st.columns(colsize)
     if i.endswith('pdf'):        
         cols[0].image(pdf)
     else:
