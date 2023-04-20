ac1da21f720b980aab4538ccaecb3d6da5cdb543
malek-zz <a.elguesaoui@gmail.com>
2023-04-20 17:08:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,14 +4,14 @@ from PIL import Image
 image = Image.open('pdf.png')
 
 
-col0, col1, col2, col3 = st.columns([1,3,3,3],gap="small")
+col0, col1, col2, col3 = st.columns([0.5,3,3,3],gap="small")
 
 with col0:
    st.subheader("test")
    st.image(image, width=15)
 with col1:
    st.subheader("Documents")
-   st.image(image, width=15) + st.text('This is some text. This is some text. This is some text.')
+   st.text('This is some text. This is some text. This is some text.')
    st.text('This is some text.')
    st.text('This is some text.')
    st.text('This is some text.')
