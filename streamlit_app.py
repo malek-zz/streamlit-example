46bfe96ee2e82bd19fe26a61ae3d5a8338383d11
malek-zz <a.elguesaoui@gmail.com>
2023-04-20 15:59:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 col1, col2, col3 = st.columns(3)
 
 with col1:
-   st.header("Documents")
+   st.subheader("Documents")
    st.text('This is some text.')
    st.text('This is some text.')
    st.text('This is some text.')
@@ -14,8 +14,8 @@ with col1:
    st.text('This is some text.')
 
 with col2:
-   st.header("Topics")
+   st.subheader("Topics")
    st.text('This is some text.')
 
 with col3:
-   st.header("Content")
+   st.subheader("Content")
