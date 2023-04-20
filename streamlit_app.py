487de7c1bdf2b0df5f25ecd592a315ace93b770e
malek-zz <a.elguesaoui@gmail.com>
2023-04-20 21:49:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
 from PIL import Image
 
+st.set_page_config(
+    page_title="Demo App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+
 pdf = Image.open('pdf.png')
 docx = Image.open('docx.png')
 
