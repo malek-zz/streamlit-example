f4eb51512d5e2aee1948c467ebf2115f66f7436e
malek-zz <a.elguesaoui@gmail.com>
2023-04-20 15:46:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,14 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
    st.header("Documents")
-   st.text_area("Test1","","Model Sexual Harassment Policy.pdf")
+txt = st.text_area('Text to analyze', '''
+    It was the best of times, it was the worst of times, it was
+    the age of wisdom, it was the age of foolishness, it was
+    the epoch of belief, it was the epoch of incredulity, it
+    was the season of Light, it was the season of Darkness, it
+    was the spring of hope, it was the winter of despair, (...)
+    ''')
+st.write('Sentiment:', run_sentiment_analysis(txt))
 
 with col2:
    st.header("Topics")
