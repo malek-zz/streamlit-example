cdde3898abd0665be58a50b03b7c51ea15b89f81
malek-zz <a.elguesaoui@gmail.com>
2023-04-20 16:16:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,8 @@ image = Image.open('pdf.png')
 col0, col1, col2, col3 = st.columns([1,3,3,3],gap="small")
 
 with col0:
-   st.subheader("")
-   st.image(image, width=10)
+   st.subheader("test")
+   st.image(image, width=15)
 with col1:
    st.subheader("Documents")
    st.text('This is some text. This is some text. This is some text.')
